38deb95378256570e3b98604e2b230ce43ab3fc9
zhongshizhi <zhongshizhi@ghostcloud.cn>
2018-09-25 16:15:04 +0800

    数据库修改

--- thinkphp/ThinkPHP/Conf/convention.php
+++ thinkphp/ThinkPHP/Conf/convention.php
@@ -54,7 +54,7 @@ return  array(
 
     /* 数据库设置 */
     'DB_TYPE'               =>  'mysql',     // 数据库类型
-    'DB_HOST'               =>  '192.168.22.133', // 服务器地址
+    'DB_HOST'               =>  'localhost', // 服务器地址
     'DB_NAME'               =>  'lol',          // 数据库名
     'DB_USER'               =>  'root',      // 用户名
     'DB_PWD'                =>  'root',          // 密码
