94246a92fc2ca906ee18e804bd00e1e731a62683
zhongshizhi <zhongshizhi@ghostcloud.cn>
2018-09-30 10:07:31 +0800

    fakeData

--- thinkphp/Application/Home/Conf/config.php
+++ thinkphp/Application/Home/Conf/config.php
@@ -2,10 +2,10 @@
 return array(
 	//'配置项'=>'配置值'
 	/* 数据库设置 */
-    'DB_TYPE'               =>  'mysql',     // 数据库类型
-    'DB_HOST'               =>  'shuxiaoxii.imwork.net',//'140.82.16.109', // 服务器地址
-    'DB_NAME'               =>  'lol',          // 数据库名
-    'DB_USER'               =>  'root',      // 用户名
-    'DB_PWD'                =>  'zsz123456',          // 密码
-    'DB_PORT'               =>  '47115',        // 端口
+    'DB_TYPE'               =>  'mysql',    
+    'DB_HOST'               =>  'localhost',
+    'DB_NAME'               =>  'lol',         
+    'DB_USER'               =>  'root',    
+    'DB_PWD'                =>  'root',         
+    'DB_PORT'               =>  '3306',
 );
