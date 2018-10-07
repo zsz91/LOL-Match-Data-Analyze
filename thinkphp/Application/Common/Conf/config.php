252d77ec65789b3ecdd595f2b14b4cfcf1661217
zsz91 <shuxiao1@qq.com>
2018-10-07 19:51:45 +0800

    数据更新

--- thinkphp/Application/Common/Conf/config.php
+++ thinkphp/Application/Common/Conf/config.php
@@ -1,4 +1,10 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+	'DB_TYPE'               =>  'mysql',     // 数据库类型
+    'DB_HOST'               =>  'localhost',
+    'DB_NAME'               =>  'lol',          // 数据库名
+    'DB_USER'               =>  'root',      // 用户名
+    'DB_PWD'                =>  '',          // 密码
+    'DB_PORT'               =>  '3306',        // 端口
 );
--- thinkphp/ThinkPHP/Conf/convention.php
+++ thinkphp/ThinkPHP/Conf/convention.php
@@ -54,7 +54,7 @@ return  array(
 
     /* 数据库设置 */
     'DB_TYPE'               =>  'mysql',     // 数据库类型
-    'DB_HOST'               =>  'localhost',//'140.82.16.109', // 服务器地址
+    'DB_HOST'               =>  'localhost',
     'DB_NAME'               =>  'lol',          // 数据库名
     'DB_USER'               =>  'root',      // 用户名
     'DB_PWD'                =>  '',          // 密码
